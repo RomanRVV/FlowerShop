1bbfa2578a8b06004164b6a202ee89d7760110da
RomanRVV <rvachyov.roma@yandex.ru>
2023-08-23 19:49:18 +0300

    Create models

--- bot/admin.py
+++ bot/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(Bouquet)
+class BouquetAdmin(admin.ModelAdmin):
+    list_display = ('name',
+                    'price')
+    search_fields = ['name', 'price']
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('user_name',
+                    'user_phone',
+                    'bouquet')
+    search_fields = ('user_phone',
+                     'bouquet',
+                     'delivery_date')
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['name']
--- bot/models.py
+++ bot/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Bouquet(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='bouquet/')
+    price = models.IntegerField()
+    events = models.ManyToManyField(Event)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    user_id = models.CharField(max_length=100, null=True)
+    user_name = models.CharField(max_length=100, null=True)
+    user_phone = models.CharField(max_length=20, null=True)
+    delivery_date = models.DateField(null=True)
+    delivery_time = models.TimeField(null=True)
+    delivery_address = models.CharField(max_length=200, null=True)
+    bouquet = models.ForeignKey(null=True, on_delete=models.SET_NULL, to='bot.Bouquet')
+
